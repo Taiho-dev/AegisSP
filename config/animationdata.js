23d57bb5aad86cebd7666875344db3f06f3f8c7d
Taiho-dev <monorom.pang91@gmail.com>
2026-09-07 00:29:05 +0200

    new files for future use

---
+++ config/animationdata.js
@@ -0,0 +1,21 @@
+module.exports = {
+Gunner:{
+	Elin:{
+		Rolling:{
+			animDuration: 1433,
+			moveDir: 0,
+			moveDistance1:65.160355,
+			moveDistance2: 133.35938,
+			moveDistance3: 150,
+			moveDistance4: 150,
+			oveDistance5: 150,.
+			moveDistance6: 50,
+			moveDistance7: 150,
+			rootMotion: true,
+			rootRotate: false,
+			rotateYaw: 0,
+			},
+		},
+	},
+};
+//Will need this later for movement/distance interpolation
---
+++ config/chaindata.js
@@ -0,0 +1,4 @@
+module.exports = {
+//Chain rules goes here //Checks for illegal chain and modifies so you don't get blocked by the server
+
+};
